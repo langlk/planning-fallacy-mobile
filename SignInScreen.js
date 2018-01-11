13b38a8668733c7b224beb3e97f3cb9dacd04770
Kelsey Langlois <kels.langlois@gmail.com>
2018-01-10 19:01:59 -0800

    Add basic sign in/up screens.

--- SignInScreen.js
+++ SignInScreen.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text, Input, AsyncStorage, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, AsyncStorage, StyleSheet } from 'react-native';
 
 export default class SignInScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  signInUser() {
+    console.log(this.state);
+  }
+
   render() {
     return(
       <View>
         <Text>Sign In</Text>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => this.setState({email})}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({password})}
+        />
+        <Button
+          onPress={() => this.signInUser() }
+          title="Submit"
+        />
       </View>
     );
   }
--- SignUpScreen.js
+++ SignUpScreen.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { View, Text, Input, AsyncStorage } from 'react-native';
+import { View, Text, TextInput, AsyncStorage, Button } from 'react-native';
 
 export default class SignUpScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    };
+  }
+
+  registerUser() {
+    console.log(this.state);
+  }
+
   render() {
     return(
       <View>
         <Text>Sign Up</Text>
+        <TextInput
+          placeholder="Username"
+          onChangeText={(name) => this.setState({name})}
+        />
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => this.setState({email})}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({password})}
+        />
+        <TextInput
+          placeholder="Password Confirmation"
+          secureTextEntry={true}
+          onChangeText={(passwordConfirmation) => this.setState({passwordConfirmation})}
+        />
+        <Button
+          onPress={() => this.registerUser() }
+          title="Submit"
+        />
       </View>
     );
   }
