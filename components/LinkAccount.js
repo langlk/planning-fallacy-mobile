d3146d13a681219b16b83f076742e70b6c4f3f75
Kelsey Langlois <kels.langlois@gmail.com>
2018-03-25 15:28:18 -0700

    Move stylesheet.

--- components/LinkAccount.js
+++ components/LinkAccount.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, Text, Linking } from 'react-native';
 
 import IP_ADDRESS from '../secrets.js';
-import styles from '../styles.js';
 
 export default class LinkAccount extends React.Component {
 
@@ -10,7 +9,7 @@ export default class LinkAccount extends React.Component {
     return (
       <View>
         <Text>It looks like you have not linked your Google Calendar yet.</Text>
-        
+
         <Text
           onPress={() => Linking.openURL(`http://${IP_ADDRESS}:3000?token=${this.props.token}`)}
           >
--- components/LoggedIn.js
+++ components/LoggedIn.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
 
-import styles from '../styles.js';
 import LinkAccount from './LinkAccount.js';
 import Dashboard from './Dashboard.js';
 
